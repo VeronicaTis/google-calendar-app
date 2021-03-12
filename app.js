32c0352d718bd33637c7032dbd89e59c3506fa11
Veronica Tisdel <vtisdel192@west-mec.local>
2021-03-12 16:15:23 -0700

    The event function works now with help from Mr.B

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 'use strict';
 const fs = require('fs');
-const bn = document.getElementById('m');
-fs.readFile('link.json', (err, data) => {
-  if (err) throw err;
-  let student = JSON.parse(data);
-  bn.innerHTML = student[0];
+const http = require('http');
+
+
+const hostname = '127.0.0.1';//server id adress
+const port = 8000;//port
+
+//Reading JSON information from try.json
+
+//console.log(eventList[0].name);
+
+
+//setting up the server host
+const server = http.createServer((req, res) => {
+    
+
+  if(req.url =='/'){
+      fs.readFile('index.html', function(err, data){
+          if(err) throw err;
+          fs.readFile('try.json', (err, data) => {
+            if (err) throw err;
+            let eventList = JSON.parse(data);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            for(var j = 0; j < eventList.length; j++){
+              res.write('<h3>Event ' +eventList.indexOf(j++) +  '</h3><p> Event Name: </br>' +eventList[j].name+ '</p>'); 
+              res.write('<p> Event Time/Date: </br>' +eventList[j].stime+ '</p>'); 
+            }
+             return res.end();
+          });
+          
+          
+      });
+  }
+  /*else if(req.url =='/about'){
+      fs.readFile('about.html', function(err, data){
+          if(err) throw err;
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          return res.end();
+      });
+  }*/
+  
+  
+
 });
 
-console.log('This is after the read call');
+//starting the server
+server.listen(port,hostname,()=>{
+  console.log('Server runnit @ ' +hostname+ ':' + port);
+});
+
--- main.js
+++ main.js
@@ -9,7 +9,8 @@ const {google} = require('googleapis');
 //const cid = document.getElementById("cid").value;
 //const tmin = document.getElementById("tmin").value;
 
-var bnsm = [];
+var eventLA = [];
+var eventIA = [];
 
 const CLIENT_ID = '190904681507-acqntr4utgm2ubj67b1n11qrbmvotns3.apps.googleusercontent.com';
 const CLIENT_SECRET ='DI-i1Vdrsq2bykrS9So83tb8';
@@ -52,15 +53,15 @@ function listEvents(auth) {
         let data = JSON.stringify(detail, null, 2);
         if (i != events.length - 1)
         {
-          bnsm += data+'\,\n';
+          eventLA += data+'\,\n';
         }
         else if (i = 1)
         {
-          bnsm += '\n'+data+'\n';
+          eventLA += '\n'+data+'\n';
         }
         else 
         {
-          bnsm += data+'\n';
+          eventLA += data+'\n';
         }
         
 
@@ -69,9 +70,72 @@ function listEvents(auth) {
     } else {
       console.log('No upcoming events found.');
     }
-    fs.writeFileSync('try.json','['+bnsm+']' );
+    fs.writeFileSync('try.json','['+eventLA+']' );
   });
 }
-listEvents(oAuth2Client);
+//listEvents(oAuth2Client);
+var eventBV = {
+'summary': 'Google I/O 2019',
+'location': '800 Howard St., San Francisco, CA 94103',
+'description': 'A chance to hear more about Googles developer products.',
+'start': {
+  'dateTime': '2021-05-28T09:00:00-07:00',
+  'timeZone': 'America/Los_Angeles',
+},
+'end': {
+  'dateTime': '2021-05-28T17:00:00-07:00',
+  'timeZone': 'America/Los_Angeles',
+},
+'attendees': [
+  {'email': 'lpage@example.com'},
+  {'email': 'sbrin@example.com'},
+],
+'reminders': {
+  'useDefault': false,
+  'overrides': [
+    {'method': 'email', 'minutes': 24 * 60},
+    {'method': 'popup', 'minutes': 10},
+  ],
+}
+};
 
 
+  //This is a standard format for creating calendar events.
+  let eventT = {
+      'summary': 'code wont work',
+      'description': 'wont work',
+      'start': {
+          'dateTime': '2021-05-28T09:00:00-07:00',     // Format: '2015-05-28T09:00:00-07:00'
+          'timeZone': 'Asia/Calcutta',
+      },
+      'end': {
+          'dateTime': '2021-05-28T09:00:00-07:00',
+          'timeZone': 'Asia/Calcutta',
+      }
+      
+  };
+
+
+function makeEvents(oAuth2Client) {
+  const accessToken = oAuth2Client.getAccessToken();
+  const Calendar1 = google.calendar({version: 'v3', oAuth2Client});
+  Calendar1.events.insert({
+    
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    refreshToken: REFRESH_TOKEN,
+    accessToken: accessToken,
+    key: API_KEY,
+    
+   auth: oAuth2Client,
+   calendarId: 'vtisdel192@west-mec.org',
+   resource: eventT,
+  }, function(err, event) {
+    if (err) {
+        console.log('There was an error contacting the Calendar service: ' + err);
+        return;
+    }
+    console.log('Event created: %s', event.data.htmlLink);
+});
+}
+  makeEvents(oAuth2Client);
